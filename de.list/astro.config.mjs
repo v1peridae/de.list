5bff8f2f4ec78f152867ca63abcaadf34148a43e
louisa :3 <louisakavindu@gmail.com>
2025-07-18 12:42:04 -0400

    you will be missed

--- de.list/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-
-export default defineConfig({
-  output: "server",
-  integrations: [react(), tailwind()],
-});
