40d2bea6bb6cedc30a93039d1866e1a16740aa79
louisa :3 <louisakavindu@gmail.com>
2025-07-20 15:36:40 -0400

    grrrr

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   output: "server",
+  adapter: vercel(),
   integrations: [react(), tailwind()],
 });
